8fc3f19a82745abdd1206025b9edac0f07608ea9
lihle-sibiya <129907260+lihle-sibiya@users.noreply.github.com>
2024-07-02 16:03:46 +0200

    Third commit

--- hello-world.py
+++ hello-world.py
@@ -5,3 +5,28 @@ myValue=1
 print(myValue)
 
 print(type(myValue))
+
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+print("# # # # # # # #       #     #    #")
+# Float Data type
+myValue1=3.14
+print(myValue1)
+print(str(myValue1) + " is of the data type " + str(type(myValue1)))
+
+print("# # # # # # # #       #     #    #")
+# Complex Data type
+
+myValue2=5j
+print(type(myValue2))
+print(str(myValue2) + " is of the data type " + str(type(myValue2)))
+
+
+print("# # # # # # # #       #     #    #")
+# Boolean Data type
+
+myValue3=True
+print(myValue3)
+print(type(myValue3))
+print(str(myValue3) + " is of the data type " + str(type(myValue3)))
+
