765ca8bf190b70bfc660579b91c82abab37ab26b
lihle-sibiya <129907260+lihle-sibiya@users.noreply.github.com>
2024-07-02 17:39:45 +0200

    Ninth commit

---
+++ guessing-game.py
@@ -0,0 +1,22 @@
+#Explain rules
+print("Welcome to Guess the Number!")
+print("The rules are simple. I will think of a number, and you will try to guess it.")
+
+import random
+number = random.randint(1,10)
+
+isGuessRight = False
+
+#If the user has not guessed the correct answer, enter the loop.
+while isGuessRight != True:
+    #Ask the user for a guess.
+    guess = input("Guess a number between 1 and 10: ")
+   #Is the guess the correct number?
+    if int(guess) == number:
+        #If the correct guess, tell the user it was the correct guess and exit the loop.
+        print("You guessed {}. That is correct! You win!".format(guess))
+        isGuessRight = True
+        #If the wrong guess, tell the user it was the wrong guess and continue the loop
+    else:
+        print("You guessed {}. Sorry, that isn’t it. Try again.".format(guess))
+        
