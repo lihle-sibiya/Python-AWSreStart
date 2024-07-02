38295e6f721f893222413c0dc407709a9f1d62dc
lihle-sibiya <129907260+lihle-sibiya@users.noreply.github.com>
2024-07-02 16:21:52 +0200

    Fourth commit

--- hello-world.py
+++ hello-world.py
@@ -30,3 +30,51 @@ print(myValue3)
 print(type(myValue3))
 print(str(myValue3) + " is of the data type " + str(type(myValue3)))
 
+
+print("# # # # # # # #       #     #    #")
+# String Data type
+
+myString = "This is a string."
+print(myString)
+print(type(myString))
+#Str- return value into a string
+print(myString + " is of the data type " + str(type(myString)))
+
+print("# # # # # # # #       #     #    #")
+#Concanate
+firstString = "water"
+secondString = "fall"
+thirdString = firstString + secondString
+print(thirdString)
+
+#name = input("What is your name? ")
+#print(name)
+
+print("# # # # # # # #       #     #    #")
+#Lists
+myFruitList = ["apple", "banana", "cherry"]
+print(myFruitList)
+print(type(myFruitList))
+
+print(myFruitList[0])
+print(myFruitList[1])
+print(myFruitList[2])
+
+print("# # # # # # # #       #     #    #")
+#Lists - changing value
+myFruitList[2] = "orange"
+print(myFruitList)
+
+
+print("# # # # # # # #       #     #    #")
+# Tuple Data type
+myFinalAnswerTuple = ("apple", "banana", "pineapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+
+print(myFinalAnswerTuple[0])
+print(myFinalAnswerTuple[1])
+print(myFinalAnswerTuple[2])
+
+print("# # # # # # # #       #     #    #")
+# Dictionary
